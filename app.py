e9251682e41158f69cc1d70ce03dccd2282545ee
Ebnou Abdem Seyid Abdellahi <65211672+Seyid2020@users.noreply.github.com>
2025-10-17 16:00:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.markdown("""
 
 st.markdown("""
 <iframe 
-    src="https://apps.abacus.ai/chatllm/?appId=8b66e9b1e&hideTopBar=2"
+    src="https://api.abacus.ai/api/v0/getChatBotWidgetSDKLink?externalApplicationId=8b66e9b1e"
     width="420"
     height="600"
     style="border: 2px solid #006666; border-radius: 12px;"
